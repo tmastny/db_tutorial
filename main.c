d31ffdb88f458ead09836795f859f12e72bbac7f
Tim Mastny <tim.mastny@wave.com>
2023-02-01 12:36:33 -0600

    save

--- main.c
+++ main.c
@@ -21,8 +21,10 @@ typedef enum {
 } MetaCommandResult;
 
 typedef enum {
-  PREPARE_SYNTAX_ERROR,
   PREPARE_SUCCESS,
+  PREPARE_NEGATIVE_ID,
+  PREPARE_STRING_TOO_LONG,
+  PREPARE_SYNTAX_ERROR,
   PREPARE_UNRECOGNIZED_STATEMENT,
 } PrepareResult;
 
@@ -36,8 +38,8 @@ typedef enum {
 
 typedef struct {
   uint32_t id;
-  char username[COLUMN_USERNAME_SIZE];
-  char email[COLUMN_EMAIL_SIZE];
+  char username[COLUMN_USERNAME_SIZE + 1];
+  char email[COLUMN_EMAIL_SIZE + 1];
 } Row;
 
 typedef struct {
@@ -70,15 +72,15 @@ void print_row(Row* row) {
 }
 
 void serialize_row(Row* source, void* destination) {
-  memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+  memcpy(destination + ID_OFFSET,       &(source->id),       ID_SIZE);
   memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
-  memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+  memcpy(destination + EMAIL_OFFSET,    &(source->email),    EMAIL_SIZE);
 }
 
 void deserialize_row(void* source, Row* destination) {
-  memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+  memcpy(&(destination->id),       source + ID_OFFSET,       ID_SIZE);
   memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
-  memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+  memcpy(&(destination->email),    source + EMAIL_OFFSET,    EMAIL_SIZE);
 }
 
 void* row_slot(Table* table, uint32_t row_num) {
@@ -145,17 +147,36 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
   }
 }
 
+PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement) {
+  statement->type = STATEMENT_INSERT;
+
+  char* keyword = strtok(input_buffer->buffer, " ");
+  char* id_string = strtok(NULL, " ");
+  char* username = strtok(NULL, " ");
+  char* email = strtok(NULL, " ");
+
+  if (id_string == NULL || username == NULL || email == NULL) {
+    return PREPARE_SYNTAX_ERROR;
+  }
+
+  int id = atoi(id_string);
+  if (id < 0) {
+    return PREPARE_NEGATIVE_ID;
+  }
+  if (strlen(username) > COLUMN_USERNAME_SIZE) {
+    return PREPARE_STRING_TOO_LONG;
+  }
+
+  statement->row_to_insert.id = id;
+  strcpy(statement->row_to_insert.username, username);
+  strcpy(statement->row_to_insert.email, email);
+
+  return PREPARE_SUCCESS;
+}
+
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
   if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
-    statement->type = STATEMENT_INSERT;
-    int args_assigned = sscanf(
-      input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
-      statement->row_to_insert.username, statement->row_to_insert.email
-    );
-    if (args_assigned < 3) {
-      return PREPARE_SYNTAX_ERROR;
-    }
-    return PREPARE_SUCCESS;
+    return prepare_insert(input_buffer, statement);
   }
   if (strcmp(input_buffer->buffer, "select") == 0) {
     statement->type = STATEMENT_SELECT;
@@ -216,6 +237,12 @@ int main(int argc, char* argv[]) {
     switch (prepare_statement(input_buffer, &statement)) {
       case (PREPARE_SUCCESS):
         break;
+      case (PREPARE_NEGATIVE_ID):
+        printf("ID must be positive.\n");
+        continue;
+      case (PREPARE_STRING_TOO_LONG):
+        printf("String is too long.\n");
+        continue;
       case (PREPARE_SYNTAX_ERROR):
         printf("Syntax error. Could not parse statement.\n");
         continue;
--- test_db.py
+++ test_db.py
@@ -4,18 +4,60 @@ from subprocess import run, PIPE, Popen
 def run_script(commands):
     input = "\n".join(commands + [""]).encode()
     boutput = run(["./a.out"], input=input, stdout=PIPE).stdout
-    return boutput.decode('utf-8').split('\n')
+    return boutput.decode("utf-8").split("\n")
+
 
 def test_inserts_and_retreives_row():
-    result = run_script([
-      "insert 1 user1 person1@example.com",
-      "select",
-      ".exit"
-    ])
+    result = run_script(["insert 1 user1 person1@example.com", "select", ".exit"])
 
     assert result == [
+        "db > Executed.",
+        "db > (1, user1, person1@example.com)",
+        "Executed.",
+        "db > ",
+    ]
+
+
+def test_prints_error_message_when_table_is_full():
+    script = [f"insert {i} user{i} person{i}@example.com" for i in range(1, 1402)]
+    result = run_script(script + [".exit"])
+    assert result[-2] == "db > Error: Table full."
+
+
+def test_allows_inserting_strings_max_length():
+    long_username = "a" * 32
+    long_email = "a" * 255
+    script = [f"insert 1 {long_username} {long_email}", "select", ".exit"]
+    result = run_script(script)
+    assert result == [
+        "db > Executed.",
+        f"db > (1, {long_username}, {long_email})",
+        "Executed.",
+        "db > ",
+    ]
+
+
+def test_prints_error_message_if_strings_too_long():
+    long_username = "a" * 33
+    long_email = "a" * 256
+    script = [f"insert 1 {long_username} {long_email}", "select", ".exit"]
+    result = run_script(script)
+    assert result == [
+        "db > String is too long.",
+        "db > Executed.",
+        "db > ",
+    ]
+
+
+def test_prints_error_if_id_negative():
+    script = [
+      "insert -1 tim tim@tim.com",
+      "select",
+      ".exit"
+    ]
+    result = run_script(script)
+    assert result == [
+      "db > ID must be positive.",
       "db > Executed.",
-      "db > (1, user1, person1@example.com)",
-      "Executed.",
-      "db > ",
+      "db > "
     ]
