ade1285a6cda17c969b72d7438a1475f47a24aaa
Tim Mastny <tim.mastny@wave.com>
2023-02-01 21:01:52 -0600

    save

--- test_db.py
+++ test_db.py
@@ -98,10 +98,10 @@ def test_can_print_out_one_node_btree():
         "db > Executed.",
         "db > Executed.",
         "db > Tree:",
-        "leaf (size 3)",
-        "  - 0 : 1",
-        "  - 1 : 2",
-        "  - 2 : 3",
+        "- leaf (size 3)",
+        "  - 1",
+        "  - 2",
+        "  - 3",
         "db > ",
     ]
 
@@ -120,4 +120,32 @@ def test_print_error_if_duplicate_id():
         "db > (1, user1, person1@example.com)",
         "Executed.",
         "db > ",
+    ]
+
+
+def test_allows_printing_of_3_leaf_node_btree():
+    script = [f"insert {i} user{i} person{i}@example.com" for i in range(1, 15)]
+    script.extend([".btree", "insert 15 user15 person15@example.com", ".exit"])
+    result = run_script(script)
+    assert result == [
+        "db > Tree:",
+        "- internal (size 1)",
+        "  - leaf (size 7)",
+        "    - 1",
+        "    - 2",
+        "    - 3",
+        "    - 4",
+        "    - 5",
+        "    - 6",
+        "    - 7",
+        "  - key 7",
+        "  - leaf (size 7)",
+        "    - 8",
+        "    - 9",
+        "    - 10",
+        "    - 11",
+        "    - 12",
+        "    - 13",
+        "    - 14",
+        "db > Need to implement searching an internal node",
     ]
